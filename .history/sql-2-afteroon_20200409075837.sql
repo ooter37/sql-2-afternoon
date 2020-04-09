4195adcd1053513ea3b9f7b92d7bc43314c27210
Derek Lamarr <derek@macbook-pro.lan>
2020-04-09 08:16:06 -0700

    Group By section complete

--- .history/sql-2-afteroon_20200409075837.sql
+++ .history/sql-2-afteroon_20200409075837.sql
@@ -88,11 +88,3 @@ SELECT genre.name, COUNT(*) FROM track
 JOIN genre ON track.genre_id = genre.genre_id
 GROUP BY genre.name
 -- 2.
-SELECT genre.name, COUNT(*) FROM track
-JOIN genre ON track.genre_id = genre.genre_id
-WHERE genre.name = 'Pop' OR genre.name = 'Rock'
-GROUP BY genre.name;
--- 3.
-SELECT  artist.name, COUNT (*) FROM artist
-JOIN album ON artist.artist_id = album.artist_id
-GROUP BY artist.name;
